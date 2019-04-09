4bfc41ee35f89afbe0152d7a1faa3b7d1c718f41
Daniell Toidy <danielltoidy@MacBook-Pro.local>
2019-04-10 01:47:54 +0800

    Include production docker for production. :)

--- dockerfile/prod/web/entrypoint.sh
+++ dockerfile/prod/web/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 echo "Running command '$*'"
 exec /bin/bash -c "$*"
---
+++ project/project/settings/prod.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0',]
+
+#Database Setting
+#DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql', #django.db.backends.mysql 
+    #    'NAME': 'db', #local: libraries #server: 
+    #    'USER': 'dbuser', #root #root
+    #   'PASSWORD': 'dbuser112211', #local: root #server: 
+    #    'HOST': 'db', #local: localhost  #server:
+    #    'PORT': '3306',
+    #}
+#}
