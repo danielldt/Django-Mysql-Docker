faf7159c8cde119c9253a10c57c1772af7e40259
danielldt <49337080+danielldt@users.noreply.github.com>
2019-04-10 02:15:34 +0800

    Merge pull request #2 from danielldt/production
    #1 
    -Added production

--- project/project/settings/dev.py
+++ project/project/settings/dev.py
@@ -5,9 +5,9 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', #django.db.backends.mysql 
-        'NAME': 'reublydb', #local: libraries #server: 
-        'USER': 'reubly', #root #root
-        'PASSWORD': 'reubly112211', #local: root #server: 
+        'NAME': 'db', #local: libraries #server: 
+        'USER': 'dbuser', #root #root
+        'PASSWORD': 'dbuser112211', #local: root #server: 
         'HOST': 'db', #local: localhost  #server:
         'PORT': '3306',
     }
